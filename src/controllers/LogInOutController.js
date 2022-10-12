7ac6d2e6a2f5a45df5c18f21810d5b9a2f9ebb04
duongtanton <20120598@student.hcmus.edu.vn>
2022-10-12 18:44:28 +0700

    login logout

--- src/controllers/LogInOutController.js
+++ src/controllers/LogInOutController.js
@@ -12,16 +12,19 @@ const LogInOut = {
   },
 
   async create(req, res, next) {
-    const { Users } = db;
-    const { username, password, repassword } = req.body;
+    const { Users, Users_Roles, Roles } = db;
+    const { username, password, repassword, enterprise } = req.body;
     const user = await Users.findOne({ where: { username } });
     if (user != null) {
-      ;
       res.render("./login", Response(res, 0, Message(MESSAGE.ERROR, "Username already exist"), null));
     } else if (repassword != password) {
       res.render("./login", Response(res, 0, Message(MESSAGE.ERROR, "Password and retype not match"), null));
     } else {
       const user = await Users.create({ username, password }).then(result => result.toJSON?.());
+      if (enterprise) {
+        const role = await Roles.findOne({ where: { code: ROLES.ENTERPRISE } }).then(result => result.toJSON());
+        Users_Roles.create({ userId: user.id, roleId: role.id });
+      }
       const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: 60 * 60 });
       res.cookie("auth", token);
       res.render("./login", Response(res, 1, Message(MESSAGE.SUCCESS, "Register successfully. Please login!!!"), null));
@@ -46,8 +49,8 @@ const LogInOut = {
       const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: 60 * 60 });
       res.cookie("auth", token);
       const { id } = user;
-      const user_role = await Users_Roles.findAll({ where: { userId: id }, include: Roles }).then(result => result.map(ur => ur.toJSON()))
-      const roleAdmin = user_role.some(ur => (ur?.Role?.code == ROLES.AMDIN || ur?.Role?.code === ROLES.STAFF));
+      const user_role = await Users_Roles.findAll({ where: { userId: id }, include: Roles }).then(result => result.map(ur => ur.toJSON()));
+      const roleAdmin = user_role.some(ur => (ur?.Role?.code == ROLES.AMDIN || ur?.Role?.code === ROLES.STAFF || ROLES.ENTERPRISE));
       if (roleAdmin) {
         res.redirect("/admin");
       } else {
